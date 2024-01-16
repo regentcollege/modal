3e2ae6e4eb3ad4d55733ec4ab7341cc09131dfcc
Cam <ctucker@regent-college.edu>
2024-01-16 14:25:44 -0600

    Update modal.blade.php

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -49,7 +49,6 @@
                     <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ $id }}">
                         @livewire($component['name'], $component['arguments'], key($id))
                     </div>
-                @empty
                 @endforelse
             </div>
         </div>
