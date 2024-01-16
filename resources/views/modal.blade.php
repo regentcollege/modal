e5e01e27e419898c547a80125360eee48601725c
Cam <ctucker@regent-college.edu>
2024-01-16 15:06:55 -0600

    Update modal.blade.php

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -46,8 +46,8 @@
                     aria-modal="true"
             >
                 @foreach($components as $id => $component)
-                    <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ time().$id }}">
-                        @livewire($component['name'], $component['arguments'], key(time().$id))
+                    <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ $id }}">
+                        @livewire($component['name'], $component['arguments'], key($id))
                     </div>
                 @endforeach
             </div>
