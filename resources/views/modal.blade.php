be6dfc54b1dd303a56498b173be0e0731ab6bcda
Cam <ctucker@regent-college.edu>
2024-01-19 20:00:11 -0600

    Update modal.blade.php

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -47,7 +47,7 @@
             >
                 @foreach($components as $id => $component)
                     <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ $id }}">
-                        @livewire($component['name'], $component['arguments'], key($id))
+                        @livewire($component['name'], $component['arguments'], key(time().'_'.$id))
                     </div>
                 @endforeach
             </div>
