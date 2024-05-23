d02cbaadde4f006d3add66ce342ea55d3b1b64fe
Cam <ctucker@regent-college.edu>
2024-05-23 11:26:47 -0500

    Update modal.blade.php

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -53,19 +53,4 @@
             </div>
         </div>
     </div>
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const modalContainer = document.getElementById('modal-container');
-            if (modalContainer) {
-                const elements = modalContainer.querySelectorAll('[x-ref]');
-                elements.forEach(element => {
-                    if (!element) {
-                        console.error('Element not found');
-                    }
-                });
-            } else {
-                console.error('Modal container not found');
-            }
-        });
-    </script>
 </div>
